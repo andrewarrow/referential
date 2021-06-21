ee8aef0858d1fa776fe7af6faada97ddedb7dd75
Andrew Arrow <oneone@gmail.com>
2021-06-21 07:45:53 -0700

    adding --case options

--- main.go
+++ main.go
@@ -2,33 +2,54 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/andrewarrow/cloutcli"
 )
 
 func main() {
-	// case 1, anonymous, hitting bitclout.com api for small amount of data
-	fmt.Println("list messages from the global feed...")
+	if len(os.Args) == 1 {
+		fmt.Println("")
+		fmt.Println("This is a referential implementation of a simple app")
+		fmt.Println("that uses cloutcli.")
+		fmt.Println("")
+		fmt.Println("Case 1. bitclout.com api with no username")
+		fmt.Println("Case 2. bitclout.com api with username, READ-ONLY access")
+		fmt.Println("Case 3. query a copy of the complete badgerdb database (>70 GB)")
+		fmt.Println("Case 4. import badgerdb database to sqlite, then query sqlite")
+		fmt.Println("Case 5. bitclout.com api with username, WRITE access")
+		fmt.Println("")
+		fmt.Println("./referential --case1")
+		fmt.Println("./referential --case2")
+		fmt.Println("./referential --case3")
+		fmt.Println("")
+		return
+	}
 
-	list := cloutcli.GlobalPosts()
-	fmt.Printf("    Got back %d posts.\n", len(list))
-	fmt.Printf("    First one:\n")
-	PrintPost(list[0].Body)
+	flavor := os.Args[1]
 
-	// case 2, username with read-only access
-	// same as above but to show following feed vs global need a "who" is viewing
-
-	fmt.Println("\nlist messages from a user's following feed...")
-	list = cloutcli.FollowingFeedPosts("andrewarrow")
-	fmt.Printf("    Got back %d posts.\n", len(list))
-	fmt.Printf("    First one:\n")
-	PrintPost(list[0].Body)
-
-	// case 3, query a copy of the complete badgerdb database (>70 GB)
-	// you'll have to spend 24 hours first downloading it or
-	// get a copy from someone who already did that
-	cloutcli.PrintAllPostsFromBadger("../acopy/badgerdb")
+	if flavor == "--case1" {
+		// case 1, anonymous, hitting bitclout.com api for small amount of data
+		fmt.Println("list messages from the global feed...")
+		list := cloutcli.GlobalPosts()
+		fmt.Printf("    Got back %d posts.\n", len(list))
+		fmt.Printf("    First one:\n")
+		PrintPost(list[0].Body)
+	} else if flavor == "--case2" {
+		// case 2, username with read-only access
+		// same as above but to show following feed vs global need a "who" is viewing
+		fmt.Println("\nlist messages from a user's following feed...")
+		list := cloutcli.FollowingFeedPosts("andrewarrow")
+		fmt.Printf("    Got back %d posts.\n", len(list))
+		fmt.Printf("    First one:\n")
+		PrintPost(list[0].Body)
+	} else if flavor == "--case3" {
+		// case 3, query a copy of the complete badgerdb database (>70 GB)
+		// you'll have to spend 24 hours first downloading it or
+		// get a copy from someone who already did that
+		cloutcli.PrintAllPostsFromBadger("../acopy/badgerdb")
+	}
 
 	// case 4, import parts of that complete badgerdb database into a local
 	// sqlite database first, then query that sqlite database
