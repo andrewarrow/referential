49f7b8b1a291990ff945a1a6e4a9e1f48ceea561
Andrew Arrow <oneone@gmail.com>
2021-06-21 05:52:36 -0700

    listing global then following

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module referential
 
 go 1.16
 
-require github.com/andrewarrow/cloutcli v0.0.0-20210621113403-bd91a2a8c331
+require github.com/andrewarrow/cloutcli v0.0.0-20210621124736-105e3cd4786d
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/andrewarrow/cloutcli"
 )
@@ -11,22 +12,34 @@ func main() {
 	fmt.Println("list messages from the global feed...")
 
 	list := cloutcli.GlobalPosts()
-
-	for _, post := range list {
-		fmt.Println(post.Body)
-	}
+	fmt.Printf("    Got back %d posts.\n", len(list))
+	fmt.Printf("    First one:\n")
+	PrintPost(list[0].Body)
 
 	// case 2, username with read-only access
 	// same as above but to show following feed vs global need a "who" is viewing
 
-	// case 3, username with write access
-	// to post a message or send coin, cli needs private key i.e seed words
+	fmt.Println("\nlist messages from a user's following feed...")
+	list = cloutcli.FollowingFeedPosts("andrewarrow")
+	fmt.Printf("    Got back %d posts.\n", len(list))
+	fmt.Printf("    First one:\n")
+	PrintPost(list[0].Body)
 
-	// case 4, query a copy of the complete badgerdb database (>70 GB)
+	// case 3, query a copy of the complete badgerdb database (>70 GB)
 	// you'll have to spend 24 hours first downloading it or
 	// get a copy from someone who already did that
 
-	// case 5, import parts of that complete basgerdb database into a local
+	// case 4, import parts of that complete basgerdb database into a local
 	// sqlite database first, then query that sqlite database
 
+	// case 5, username with write access
+	// to post a message or send coin, cli needs private key i.e seed words
+
+}
+
+func PrintPost(body string) {
+	tokens := strings.Split(body, "\n")
+	for _, line := range tokens {
+		fmt.Println("   ", line)
+	}
 }
